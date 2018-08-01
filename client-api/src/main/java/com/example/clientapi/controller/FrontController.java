8c04fef5bdfc23adfb63a19128757fa77a9f5029
한주희 <lovia98@naver.com>
2018-08-01 17:38:27 +0900

    # spring-cloud-bus 설정 추가

--- client-api/src/main/java/com/example/clientapi/controller/FrontController.java
+++ client-api/src/main/java/com/example/clientapi/controller/FrontController.java
@@ -3,7 +3,6 @@ package com.example.clientapi.controller;
 import com.example.clientapi.properties.BasicConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- client-api/src/main/java/com/example/clientapi/controller/FrontDynamicController.java
+++ client-api/src/main/java/com/example/clientapi/controller/FrontDynamicController.java
@@ -4,7 +4,6 @@ import com.example.clientapi.properties.BasicConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
